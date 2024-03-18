2a09aa3096985d90098dbd0243987c6139e7ee1f
irfanbabbar <159956116+irfanbabbar@users.noreply.github.com>
2024-03-18 10:36:10 -0700

    sample culculater
    i made in typescript and node.js , inquirer this is my 1st project

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+    message: "enter first number", type: "number", name: "firstNumber"},
+   {message: "enter second number", type: "number", name: "secondNumber"},
+  
+   { message: "select one of the operators tp perfrom operator",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "dividion"],
+  }
+])
+// conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if (answer.operator === "multiplication"){ 
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if (answer.operator === "division") {
+            console.log(answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("Please select valid operator");
+}
